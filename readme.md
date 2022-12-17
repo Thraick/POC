7de3ecd7db79ecb34e9ae270539a0a5888458ce6
Tharick <69114159+Thraick@users.noreply.github.com>
2022-12-17 11:25:57 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,98 @@
+### Entity 
+
+-month: march, december
+-number: 2315555, 2316666
+-account: gtt, dsl, landline
+
+### Intent
+
+-Faqs
+-Outstanding Balance
+-Invoice
+-Payment History
+
+
+
+Example 
+
+## Faqs
+How do I setup Voicemail
+Where can I pay my bill
+How to change my wifi password
+How do I transfer my account to a different owner or address?
+How do I activate roaming?
+
+## Outstanding Balance
+I would like to check my outstanding Balance
+my number is 2315555
+I would like to check my gtt account
+or
+I would like to check my balance. my number is 2316666
+
+## Invoice
+i would like to check my invoice for march
+
+
+## Payment History
+I would like to check my bill for december
+
+
+
+
+**** Change ****
+
+utils/data/state.json // response
+utils/data/state/accounts.json // outstanding balance
+utils/data/state/invoice.json // invoice
+utils/data/state/payment_history.json // payment history
+
+utils/data/bi_train.json // bi_encoder
+utils/data/tfm_train.json // tfm_ner
+utils/data/faq.json // faq_answer
+
+// update globals.jac if you change tfm_ner or the path
+
+utils/model/local/twilio_bot.py // follow instruction at the bottom of file
+
+
+Jsserv makemigrations base
+Jsserv migrate
+Jsserv runserver 0.0.0.0:8008
+
+Jsserv createsuperuser
+
+login http://0.0.0.0:8008/
+
+actions load module jaseci_ai_kit.use_qa
+actions load module jaseci_ai_kit.bi_enc
+actions load module jaseci_ai_kit.tfm_ner
+
+actions load local utils/model/local/flow.py
+actions load local utils/model/local/twilio_bot.py
+
+// graph delete active:graph
+jac build main.jac
+graph create -set_active true
+sentinel register -set_active true -mode ir main.jir
+
+walker run init
+
+
+
+### bi_enc
+walker run bi_enc_train -ctx "{\"train_file\": \"utils/data/bi_train.json\"}"
+walker run bi_enc_infer -ctx "{\"labels\": [\"faq_root\", \"greetings\", \"payment due\", \"invoice\", \"payment_history\"]}"
+walker run bi_enc_save_model -ctx "{\"model_path\": \"bi_enc_model\"}"
+walker run bi_enc_load_model -ctx "{\"model_path\": \"bi_enc_model\"}"
+walker run bi_enc_delete
+
+
+### tfm_ner
+walker run tfm_ner_train -ctx "{\"train_file\": \"utils/data/tfm_train.json\"}"
+walker run tfm_ner_infer -ctx "{\"labels\": [\"number\",\"accountname\",\"month\"]}"
+walker run tfm_ner_save_model -ctx "{\"model_path\": \"tfm_ner_model\"}"
+walker run tfm_ner_load_model -ctx "{\"model_path\": \"tfm_ner_model\"}"
+walker run tfm_ner_delete
+
+
+
---
+++ utils/model/local/flow.py
@@ -0,0 +1,197 @@
+import random
+from jaseci.actions.live_actions import jaseci_action  # step 1
+import json
+import os
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+# response = selected response
+# my_dict = info json
+
+# @jaseci_action(act_group=["flow"], allow_remote=True)
+# def gen_response(response: str, my_dict: dict):
+#     answer = ""
+#     if "{{" in response:
+#         l1 = response.replace('{{', '{')
+#         l2 = l1.replace('}}', '}')
+#         answer = l2.format(**my_dict)
+#     else:
+
+#         answer = response
+
+#     return answer
+
+
+# state_ext_item = state item to be extracted with response
+# state_response = response for state
+# dial_context = extracted item to info json or dial context
+
+# return name of response and response
+
+@jaseci_action(act_group=["flow"], allow_remote=True)
+def select_response(state_ext_item: dict, state_response: list, dial_context: dict):
+# def select_response(state_ext_item: dict, state_response: list, dial_context: dict, info_response:dict):
+
+    response_name = ""
+    response = ""
+    # if (info_response):
+    #     for item in info_response:
+    #         response_name = item
+    #         response = random.choice(info_response[item])
+    if (state_ext_item):
+        context_key = list(dial_context.keys())
+        for item in state_ext_item:
+            if item not in context_key:
+                response_name = item
+                response = random.choice(state_ext_item[item])
+                break
+    if response == "":
+        response = random.choice(state_response)
+    return [response_name, response]
+
+
+# info_file = name of info file
+# dial_context = extracted item
+# info_items = items to return info about
+
+# return a dict of info
+
+# @jaseci_action(act_group=["flow"], allow_remote=True)
+# def info_json(info_file: str, dial_context: dict, info_items):
+#     # open_json = str(dir_path)+'/data/'+info_file
+#     open_json = info_file
+
+#     my_dict = {}
+#     info_id = info_items[0]
+
+#     for item in dial_context:
+#         my_dict[item] = dial_context[item][0]
+
+#     if (info_file):
+#         with open(open_json) as f:
+#             data_set = json.load(f)
+#         for data in data_set:
+#             if data[info_id] in dial_context[info_id]:
+#                 for item in info_items:
+#                     my_dict[item] = data[item]
+#     return my_dict
+
+
+
+@jaseci_action(act_group=["flow"], allow_remote=True)
+def info_json(info_file: str, dial_context: dict, info_items):
+    # open_json = str(dir_path)+'/data/'+info_file
+    open_json = info_file
+
+    my_dict = {}
+    info_id = info_items[0]
+    my_list = []
+
+    for item in dial_context:
+        my_dict[item] = dial_context[item][0]
+
+    # print("my_dic\n\n")
+    # print(my_dic)
+    if (info_file):
+        with open(open_json) as f:
+            data_set = json.load(f)
+        for data in data_set:
+            if data[info_id] in dial_context[info_id]:
+                my_dic = {}
+                for item in info_items:
+                    my_dic[item] = data[item]
+                my_list.append(my_dic)
+    
+    # print("my_dict\n\n")
+    # print(my_dic)
+    print(info_id)
+    print(my_list)
+    my_dict["info_json"]= my_list
+    return my_dict
+
+
+
+@jaseci_action(act_group=["flow"], allow_remote=True)
+def collect_info(collect_info: dict, my_dict: dict):
+
+    dict_key = list(my_dict.keys())
+
+
+    for key, value in collect_info.items():
+        if key not in dict_key:
+            return [key, value]
+    
+    return ["",""]
+
+
+
+@jaseci_action(act_group=["flow"], allow_remote=True)
+def gen_response(response: str, my_dict: dict, info_items:list):
+    
+    
+    answer = ""
+
+    # if(type(info_items) == list and "{{" in response):
+    #     my_lis = []
+    #     item = info_items[-1]
+
+    #     for a in my_dict['info_json']:
+    #         my_lis.append(a[item])
+        
+    #     lis1 =', '.join(map(str, my_lis[:-1]))
+    #     lis2 = my_lis[-1]
+
+    #     new_dict= my_dict.copy()
+    #     new_dict["first_"+item]=lis1
+    #     new_dict["last_"+item]=lis2
+    #     new_dict["num_"+item]= len(my_dict['info_json'])+1
+    #     print('new dict\n')
+    #     print(response)
+    #     print(new_dict)
+
+
+    #     l1 = response.replace('{{', '{')
+    #     l2 = l1.replace('}}', '}')
+    #     answer = l2.format(**new_dict)
+    #     # answer = 'test'
+
+    
+    if "{{" in response:
+        l1 = response.replace('{{', '{')
+        l2 = l1.replace('}}', '}')
+        answer = l2.format(**my_dict)
+    else:
+
+        answer = response
+
+    return answer
+
+
+@jaseci_action(act_group=["flow"], allow_remote=True)
+def select_options(response: str, my_dict: dict, info_items:list):
+    
+    my_lis = []
+    item = info_items[-1]
+
+    for a in my_dict['info_json']:
+        my_lis.append(a[item])
+    
+    lis1 =', '.join(map(str, my_lis[:-1]))
+    lis2 = my_lis[-1]
+
+    new_dict= my_dict.copy()
+    new_dict["first_"+item]=lis1
+    new_dict["last_"+item]=lis2
+    new_dict["num_"+item]= len(my_dict['info_json'])+1
+    print('new dict\n')
+    print(response)
+    print(new_dict)
+
+
+    l1 = response.replace('{{', '{')
+    l2 = l1.replace('}}', '}')
+    answer = l2.format(**new_dict)
+
+    return answer
